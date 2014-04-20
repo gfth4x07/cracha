d16e8e0d8621d188b09d2791497326154c9d47d9
Oda <oda@member.fsf.org>
2014-04-20 19:53:37 -0300

    nao pegava os parametros no stl.php

--- stl.php
+++ stl.php
@@ -2,15 +2,7 @@
 
 include "lib.php";
 
-$openscad_params=array(
-  'nome'=>'oda',
-  'tamanho_da_fonte'=>10,
-  'nome_x'=>17,
-  'nome_y'=>18,
-  'altura_do_vao_do_cabo'=>12,
-  'vao_do_cabo_y'=>17
-);
-
+$openscad_params=$_REQUEST['openscad_params'];
 $file=stl($openscad_params);
 header('Content-Description: File Transfer');
 header('Content-Type: application/vnd.ms-pkistl');
